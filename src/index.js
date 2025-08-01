268c25bcd73e72ad4b83e1c5f35c7d3d14ebbadb
camel-97 <cfmcmillan@icloud.com>
2025-08-01 15:30:58 +0100

    added menu thru dom, css basic, updated template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import "./styles.css";
 import { loadHome } from "./home.js";
- 
+import { loadMenu } from "./menu.js";
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    loadHome();
-})
+  loadHome();
+
+  const homeBtn = document.getElementById("home");
+  const menuBtn = document.getElementById("menu");
+  console.log("homeBtn:", homeBtn); // should NOT be null
+  console.log("menuBtn:", menuBtn); // should NOT be null
+
+  if (homeBtn) {
+    homeBtn.addEventListener("click", loadHome);
+  }
+
+  if (menuBtn) {
+    menuBtn.addEventListener("click", loadMenu);
+  }
+});
+
+
---
+++ src/menu.js
@@ -0,0 +1,48 @@
+export function loadMenu() {
+    const main = document.getElementById("content")
+    main.innerHTML = "";
+
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+    main.appendChild(menu);
+
+    const menuHead = document.createElement("div");
+    menuHead.classList.add("menu-head");
+    menuHead.textContent = "Sample Menu"
+    menu.appendChild(menuHead);
+
+    const menu1 = document.createElement("div");
+    menu1.classList.add("menu-content");
+    menu1.textContent = "Charred Hispi Cabbage, brown butter miso, smoked almond praline, burnt lemon crème fraiche     10"
+    menu.appendChild(menu1);
+
+    const menu2 = document.createElement("div");
+    menu2.classList.add("menu-content");
+    menu2.textContent = "Isle of Wight Tomatoes, pickled green strawberries, whipped burrata, lovage oil, sourdough crumb     9"
+    menu.appendChild(menu2);
+
+    const menu3 = document.createElement("div");
+    menu3.classList.add("menu-content");
+    menu3.textContent = "Hand-Dived Scallops, grilled over the shell, fennel pollen, elderflower brown butter, crispy chicken skin     14"
+    menu.appendChild(menu3);
+
+    const menu4 = document.createElement("div");
+    menu4.classList.add("menu-content");
+    menu4.textContent = "Hay-Smoked Duck Breast, plum lacquer, grilled radicchio, hazelnut & cocoa nib crumb      14"
+    menu.appendChild(menu4);
+
+    const menu5 = document.createElement("div");
+    menu5.classList.add("menu-content");
+    menu5.textContent = "Wild Mushroom & Nduja Flatbread, truffle pecorino, lemon thyme, smoked garlic yoghurt      12"
+    menu.appendChild(menu5);
+
+    const menu6 = document.createElement("div");
+    menu6.classList.add("menu-content");
+    menu6.textContent = "Heritage Beets Three Ways, salt-baked, pickled & raw — with goat’s curd, black garlic, puffed wild rice     12"
+    menu.appendChild(menu6);
+
+    const menu7 = document.createElement("div");
+    menu7.classList.add("menu-content");
+    menu7.textContent = "Aged Beef Tartare, bone marrow toast, pickled shallots, cured yolk, nasturtium     16"
+    menu.appendChild(menu7);
+};
